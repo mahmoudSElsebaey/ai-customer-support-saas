a6096de1ce94e13204a450484c65d105e105ad40
Mahmoud Salah Elsebaey <91635205+mahmoudSElsebaey@users.noreply.github.com>
2026-09-02 21:29:38 +0300

    feat: export all Mongoose models from models/index

---
+++ server/src/models/index.ts
@@ -0,0 +1,17 @@
+export { Organization, type IOrganization, type IOrganizationDocument } from "./Organization.js";
+export { User, type IUser, type IUserDocument } from "./User.js";
+export { RefreshToken, type IRefreshToken, type IRefreshTokenDocument } from "./RefreshToken.js";
+export { Customer, type ICustomer, type ICustomerDocument } from "./Customer.js";
+export { Ticket, type ITicket, type ITicketDocument } from "./Ticket.js";
+export { Message, type IMessage, type IMessageDocument } from "./Message.js";
+export {
+  KnowledgeArticle,
+  type IKnowledgeArticle,
+  type IKnowledgeArticleDocument,
+} from "./KnowledgeArticle.js";
+export {
+  CannedResponse,
+  type ICannedResponse,
+  type ICannedResponseDocument,
+} from "./CannedResponse.js";
+export { AIUsage, type IAIUsage, type IAIUsageDocument } from "./AIUsage.js";
